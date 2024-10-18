58fc059cdcd73b5db2b4fac1fdce903ad2004284
Mihail Makarov <m.makarov90@bk.ru>
2024-10-19 00:12:07 +0300

    добавил анимацию открытия меню

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 const buttonEl = document.getElementById('button-main')
-let dropDownMenuEl = document.getElementById('menu')
+const dropDownMenuEl = document.getElementById('menu')
+const openEL = document.querySelector('.open')
 
 buttonEl.addEventListener('click', () => {
 	dropDownMenuEl.style.display = dropDownMenuEl.style.display === 'none' ? 'flex' : 'none'
+
+	openEL.style.display = openEL.style.display === 'block' ? 'none' : 'block'
+
+	document.querySelector('.close').style.display = document.querySelector('.close').style.display === 'none' ? 'block' : 'none'
 })
+
+dropDownMenuEl.classList.toogle('animation--open-menu')
