c866b6c599cb6e2124a1d9264c7c6c4e9aeaf895
Mihail Makarov <m.makarov90@bk.ru>
2024-10-19 13:44:22 +0300

    добавил hover на svg

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
-const buttonEl = document.getElementById('button-main')
-const dropDownMenuEl = document.getElementById('menu')
-const openEL = document.querySelector('.open')
-const closeEl = document.querySelector('.close')
+const buttonEl = document.getElementById('button-main');
+const dropDownMenuEl = document.getElementById('menu');
+const openEL = document.querySelector('.open');
+const closeEl = document.querySelector('.close');
+
+// function showMenu() {
+// 	dropDownMenuEl.style.display = dropDownMenuEl.style.display === 'none' ? 'flex' : 'none';
+// }
+// buttonEl.addEventListener('click', showMenu);
 
 buttonEl.addEventListener('click', () => {
-	dropDownMenuEl.style.display = dropDownMenuEl.style.display === 'none' ? 'flex' : 'none'
+	dropDownMenuEl.style.display = dropDownMenuEl.style.display === 'none' || !dropDownMenuEl.style.display ? 'flex' : 'none';
 
-	openEL.style.display = openEL.style.display === 'block' ? 'none' : 'block'
+	openEL.style.display = openEL.style.display === 'block' ? 'none' : 'block';
 
-	closeEl.style.display = closeEl.style.display === 'none' ? 'block' : 'none'
-})
+	closeEl.style.display = closeEl.style.display === 'none' ? 'block' : 'none';
+});
