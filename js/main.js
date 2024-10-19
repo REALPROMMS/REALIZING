ca1de70251628ed1de83a099731ee7ceafe7c344
Mihail Makarov <m.makarov90@bk.ru>
2024-10-19 14:20:38 +0300

    done

--- js/main.js
+++ js/main.js
@@ -3,15 +3,10 @@ const dropDownMenuEl = document.getElementById('menu');
 const openEL = document.querySelector('.open');
 const closeEl = document.querySelector('.close');
 
-// function showMenu() {
-// 	dropDownMenuEl.style.display = dropDownMenuEl.style.display === 'none' ? 'flex' : 'none';
-// }
-// buttonEl.addEventListener('click', showMenu);
+function toggleMenu() {
+	dropDownMenuEl.classList.toggle('hidden');
+	openEL.classList.toggle('hidden');
+	closeEl.classList.toggle('hidden');
+}
 
-buttonEl.addEventListener('click', () => {
-	dropDownMenuEl.style.display = dropDownMenuEl.style.display === 'none' || !dropDownMenuEl.style.display ? 'flex' : 'none';
-
-	openEL.style.display = openEL.style.display === 'block' ? 'none' : 'block';
-
-	closeEl.style.display = closeEl.style.display === 'none' ? 'block' : 'none';
-});
+buttonEl.addEventListener('click', toggleMenu);
